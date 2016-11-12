b13c6d3667699e51d36fb277dc19cb33f46d5f02
John Wu <john.wu@mail.mcgill.ca>
2016-11-12 13:38:49 -0500

    Refactored files

--- app/routes.js
+++ app/routes.js
@@ -54,6 +54,6 @@ module.exports = function (app) {
     });
 
     app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile(__dirname + '/public/api.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 }
