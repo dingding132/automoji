99bcb6bf22b98806be801b9a6d1d0ffa96cb3700
John Wu <john.wu@mail.mcgill.ca>
2016-11-12 12:07:20 -0500

    Got emotioinal analysis working

--- app/routes.js
+++ app/routes.js
@@ -10,7 +10,40 @@ module.exports = function (app) {
     app.post('/api/kairos', function(req, res) {
         request({
             method: 'POST',
-            url: 'https://api.kairos.com/v2/media?source='+__dirname+'/public/media/happy.mov',
+            // url: req.params.url,
+            //url: 'https://api.kairos.com/v2/media?source='+__dirname+'%2Fpublic%2Fmedia%2Fhappy.mov',
+            url: 'https://api.kairos.com/v2/media?source=http%3A%2F%2Fmedia.kairos.com%2Ftest.flv',
+            timeout: '10',
+            headers: {
+                'app_id': 'bc0b9fcd',
+                'app_key': '0ae50b89bb2c96c6fd7a2c8df8e89135'
+            }}, function (error, response, body) {
+            console.log('Status:', response.statusCode);
+            console.log('Headers:', JSON.stringify(response.headers));
+            console.log('Response:', body);
+            res.json(body);
+        });
+    });
+
+    app.get('/api/kairos/:media_id', function(req,res) {
+        request({
+            method: 'GET',
+            url: 'https://api.kairos.com/v2/media/' + req.params.media_id,
+            headers: {
+                'app_id': 'bc0b9fcd',
+                'app_key': '0ae50b89bb2c96c6fd7a2c8df8e89135'
+            }}, function (error, response, body) {
+            console.log('Status:', response.statusCode);
+            console.log('Headers:', JSON.stringify(response.headers));
+            console.log('Response:', body);
+            res.json(body);
+        });
+    });
+
+    app.delete('/api/kairos/:media_id', function(req,res) {
+        request({
+            method: 'DELETE',
+            url: 'https://api.kairos.com/v2/media/' + req.params.media_id,
             headers: {
                 'app_id': 'bc0b9fcd',
                 'app_key': '0ae50b89bb2c96c6fd7a2c8df8e89135'
--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,17 +1,57 @@
 /**
  * Created by JohnWu on 2016-11-12.
  */
-// angular.module('controller', []).controller('mainController', function($scope, $http) {
-//     $scope.message = 'ffff';
-//     $http.get('/api/kairos').then(function(response) {
-//         $scope.analysis = response.data;
-//     });
-// });
 
 angular.module('controller', [])
-    .controller('mainController', function($scope, $http) {
+    .controller('mainController', function($scope, $http, $timeout) {
+
         $scope.greeting = "Hello World";
-        $http.post('/api/kairos').then(function(response) {
-            $scope.analysis = response.data;
+
+        $http.post('/api/kairos').success(function(response) {
+            $timeout( function () {
+                getResult(JSON.parse(response).id);
+                deleteResult(JSON.parse(response).id);
+            }, 8000 );
+
         });
+
+        var getResult = function (id) {
+            $http.get('/api/kairos/' + id).success(function(response) {
+                console.log("Final response", JSON.parse(response));
+
+                var anger = 0;
+                var disgust = 0;
+                var fear = 0;
+                var joy = 0;
+                var sadness = 0;
+                var surprise = 0;
+
+                for (var i=0; i<JSON.parse(response).frames.length; i++) {
+                    console.log("hi");
+                    anger += JSON.parse(response).frames[i].people[0].emotions.anger;
+                    disgust += JSON.parse(response).frames[i].people[0].emotions.disgust;
+                    fear += JSON.parse(response).frames[i].people[0].emotions.fear;
+                    joy += JSON.parse(response).frames[i].people[0].emotions.joy;
+                    sadness += JSON.parse(response).frames[i].people[0].emotions.sadness;
+                    surprise += JSON.parse(response).frames[i].people[0].emotions.surprise;
+                }
+
+                $scope.analysis = {
+                    anger: anger/JSON.parse(response).frames.length,
+                    disgust: disgust/JSON.parse(response).frames.length,
+                    fear: fear/JSON.parse(response).frames.length,
+                    joy: joy/JSON.parse(response).frames.length,
+                    sadness: sadness/JSON.parse(response).frames.length,
+                    surprise: surprise/JSON.parse(response).frames.length
+                };
+
+            });
+        }
+
+        var deleteResult = function (id) {
+            $http.delete('/api/kairos/' + id).success(function(response) {
+                console.log("Delete response", JSON.parse(response));
+            });
+        };
+
     });
