85c22ad10a18b5714a258aa5cc6f9cbde4ded324
John Wu <john.wu@mail.mcgill.ca>
2016-11-12 16:50:54 -0500

    Created api for messages

---
+++ app/models/message.js
@@ -0,0 +1,15 @@
+/**
+ * Created by JohnWu on 2016-11-12.
+ */
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('Message', {
+    author: {
+        type: String,
+        default: ''
+    },
+    text: {
+        type: String,
+        default: ''
+    }
+});
